c5f8a295db277ef0f38cbe11cc71a89a515aa643
conceptuallyflawed <conceptuallyflawed@gmail.com>
2011-03-29 22:21:14 +0000

    this works somewhat...

--- viewer.py
+++ viewer.py
@@ -14,17 +14,29 @@ class Game(object):
         self.clock = pygame.time.Clock()
         self.screen = pygame.display.set_mode((self.width,self.height))
         self.bg = pygame.image.load(self.tileimage).convert()
+    def handle_event(self,event):
+        if(event.type == pygame.KEYDOWN):
+            if(event.key == pygame.K_SPACE):
+                print "Space bar pressed down."
+            elif(event.key == pygame.K_ESCAPE):
+                print "Escape key pressed down."
+        elif(event.type == pygame.KEYUP):
+            if(event.key == pygame.K_SPACE):
+                print "Space bar released."
+            elif(event.key == pygame.K_ESCAPE):
+                print "Escape key released."
+                pygame.quit()
 
     def update(self):
         self.clock.tick(50)
+        for event in pygame.event.get():
+            self.handle_event(event)
 
     def draw(self):
-
-        for i in range(256):
-            for j in range(256):
-                self.screen.blit(self.bg,pygame.Rect(0,0,self.width,self.height),pygame.Rect(2*32,0,3*32-1,32))
-		print "added a tile"
-                
+        self.screen.fill((0,0,0))
+        for i in range(25):
+            for j in range(25):
+                self.screen.blit(self.bg,pygame.Rect(i*32,j*32,self.width,self.height),pygame.Rect(2*32,0,32,32))
 g = Game()
 while True:
     g.update()
