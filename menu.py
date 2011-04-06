5454f971dc0875fbb546bdbab824043067f65f0b
IsmirQuismik <IsmirQuismik@ccefa083-e103-f8a5-2a6a-c76eae7317cc>
2011-04-06 17:34:18 +0000

    PDD consolidated menu and menus for simplicity

--- menu.py
+++ menu.py
@@ -56,6 +56,83 @@ class Menu(object):
         return self.nextMenu
     def leave_menu(self):
         self.bInMenu = False
+
+class MainMenu(Menu):
+    def __init__(self,screen):
+        super(MainMenu, self).__init__(screen)
+        buttonlocx = 300
+        buttonlocx -= 128
+        buttonlocy = 300
+        buttonlocy -= 96
+        font = pygame.font.Font(None, 36)
+        text = font.render("", 1, (0,0,0))
+        self.add_button(buttonlocx, (buttonlocy -128), text, button.StartOnClick(), "StartCampaign")
+        self.add_button(buttonlocx,buttonlocy,text, button.MissionSelectOnClick(), "StartMission")
+        self.add_button(buttonlocx,(buttonlocy+128),text, button.SavedCampaignSelectOnClick(), "LoadCampaign")
+        self.add_button(buttonlocx,(buttonlocy+256),text, button.SavedMissionSelectOnClick(), "LoadMission")
+    
+class MissionSelectMenu(Menu):
+    def __init__(self,screen):
+        i = 0
+        super(MissionSelectMenu, self).__init__(screen)
+        filename = "mission%d.txt" % (i,)
+        initx = 300 - 128
+        inity = 300-128
+        pathstring = os.path.join("missions", filename)
+        while(os.path.isfile(pathstring)):
+            LC = button.LoadOnClick()
+            LC.setMission(pathstring)
+            font = pygame.font.Font(None, 36)
+            text = font.render(filename, 1, (10,10,10))
+            self.add_button(initx, inity +(64*i), text, LC, "Blank")
+            i += 1
+            filename = "mission%d.txt" % (i,)
+            pathstring = os.path.join("missions", filename)
+    def leave_menu(self):
+        self.nextMenu = MainMenu(self.screen)
+
+class SavedMissionSelectMenu(Menu):
+    def __init__(self,screen):
+        i = 0
+        super(SavedMissionSelectMenu, self).__init__(screen)
+        filename = "savedMission%d.txt" % (i,)
+        initx = 300 - 128
+        inity = 300-128
+        pathstring = os.path.join("savedMissions", filename)
+        while(os.path.isfile(pathstring)):
+            LC = button.LoadOnClick()
+            LC.setMission(pathstring)
+            font = pygame.font.Font(None, 36)
+            text = font.render(filename, 1, (10,10,10))
+            self.add_button(initx, inity +(64*i), text, LC, "Blank")
+            i += 1
+            filename = "savedMission%d.txt" % (i,)
+            pathstring = os.path.join("savedMissions", filename)
+    def leave_menu(self):
+        self.nextMenu = MainMenu(self.screen)
+
+class SavedCampaignSelectMenu(Menu):
+    def __init__(self,screen):
+        i = 0
+        super(SavedCampaignSelectMenu, self).__init__(screen)
+        filename = "savedCampaign%d.txt" % (i,)
+        initx = 300 - 128
+        inity = 300-128
+        pathstring = os.path.join("savedCampaigns", filename)
+        while(os.path.isfile(pathstring)):
+            LC = button.LoadOnClick()
+            LC.setMission(pathstring)
+            font = pygame.font.Font(None, 36)
+            text = font.render(filename, 1, (10,10,10))
+            self.add_button(initx, inity +(64*i), text, LC, "Blank")
+            i += 1
+            filename = "savedCampaign%d.txt" % (i,)
+            pathstring = os.path.join("savedCampaigns", filename)
+
+    def leave_menu(self):
+        self.nextMenu = MainMenu(self.screen)
+        
+
             
 
             
