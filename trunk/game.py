a807cdd5327079039430235816746384349a5810
ncoppola89@gmail.com <ncoppola89@gmail.com@ccefa083-e103-f8a5-2a6a-c76eae7317cc>
2011-04-06 17:17:58 +0000

    Made a bunch of changes in game and map_class

--- trunk/game.py
+++ trunk/game.py
@@ -9,84 +9,73 @@ class Game(object):
         pygame.display.set_caption("Project Dominion")
         self.screen = pygame.display.set_mode((self.screen_width,self.screen_height))
         self.clock = pygame.time.Clock()
-        self.mouseisdown = False
-        self.mouserect = pygame.Rect(0,0,0,0)
-        self.selection = []
-        
-        #self.clock = pygame.time.Clock()
-        #self.screen = pygame.display.set_mode((self.screen_width, self.screen_height))
+        self.mouseIsDown = False
+        self.mouseRect = pygame.Rect(0,0,0,0)
         
         self.target = soldier.Soldier(float(6.0*32),float(7.0*32))
-        self.target.image.fill((0,0,255))
+        self.target.image.fill((10,0,0))
         self.target.anim_len = 500
 
         self.map = Map_Class.Map.get()
-        self.enemies = []
-        self.bullets = []
-        for i in range(10):
-            self.enemies.append(soldier.Soldier(random.randint(0,self.screen_width),random.randint(0,self.screen_height)))
         
     def update(self):
         self.clock.tick(50)
-        delta_seconds = self.clock.get_time()/1000.0
+        deltaSeconds = self.clock.get_time()/1000.0
         
         for event in pygame.event.get():
             self.handle_event(event)
-        self.target.update(delta_seconds)
-        self.target.keep_on_screen(self.screen_width,self.screen_height)
-        #for e in self.enemies:
-        for player in self.map.unit_table:
-            for e in self.map.unit_table[player]:
-                #e.update(delta_seconds)
-                #e.keep_on_screen(self.width,self.height)        
-                newbull = e.update(delta_seconds)
-                if newbull is not None:
-                    self.bullets.append(newbull)
-                e.keep_on_screen(self.screen_width,self.screen_height)
-                e.fire_at(self.target)
-        for b in self.bullets:
-            b.update(delta_seconds)
+            
+        self.map.update(deltaSeconds)
+            
+        if self.mouseIsDown:
+            self.position2 = pygame.mouse.get_pos()
+            self.mouseRect = self.mouse_select(self.position1,self.position2)
+            
     def handle_event(self, event):
-        #if event.type == pygame.QUIT:
-        #    sys.exit(0)
-        #if event.type == pygame.KEYDOWN:
-        #    if event.key == pygame.K_ESCAPE:
-        #        sys.exit(0)
-        if(event.type == pygame.QUIT):
+        if event.type == pygame.QUIT:
             pygame.quit()
-        if(event.type == pygame.KEYDOWN):
-            if(event.key == pygame.K_ESCAPE):
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
                 pygame.quit()
-        if(event.type == pygame.MOUSEBUTTONDOWN):
-            if(event.button == 1):
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if event.button == 1:
                 self.position1 = event.pos
                 self.position2 = pygame.mouse.get_pos()
-                self.mouseisdown = True
-                self.mouserect = self.map.mouseselect(self.position1,self.position2)
-        if(event.type == pygame.MOUSEBUTTONUP):
-            if(event.button == 1):
+                self.mouseIsDown = True
+                self.mouseRect = self.mouse_select(self.position1,self.position2)
+        if event.type == pygame.MOUSEBUTTONUP:
+            if event.button == 1:
                 self.position2 = event.pos
-                self.mouserect = self.map.mouseselect(self.position1,self.position2)
-                self.selection = self.map.getgroup(self.mouserect)
-                self.mouserect = pygame.Rect(0,0,0,0)
-                self.mouseisdown = False
-        if(self.mouseisdown):
-            self.position2 = pygame.mouse.get_pos()
-            self.mouserect = self.map.mouseselect(self.position1,self.position2)
+                self.mouseRect = self.mouse_select(self.position1,self.position2)
+                self.map.select_group(self.mouseRect)
+                self.mouseRect = pygame.Rect(0,0,0,0)
+                self.mouseIsDown = False
+            
+    # Returns a properly formatted rectangle from mouse positions
+    def mouse_select(self,tuple1,tuple2):
+        if(tuple1[0] < tuple2[0]):
+            #closer to the left
+            posx = tuple1[0]
+            width = tuple2[0] - tuple1[0]
+        else:
+            posx = tuple2[0]
+            width = tuple1[0] - tuple2[0]
+        #y dimension
+        if(tuple1[1] < tuple2[1]):
+            #closer to the top
+            posy = tuple1[1]
+            height = tuple2[1] - tuple1[1]
+        else:
+            posy = tuple2[1]
+            height = tuple1[1] - tuple2[1]
+            
+        return pygame.Rect(posx,posy,width,height)
+            
     def draw(self):
         self.screen.fill((0,0,0))
-        self.map.draw()
-        self.target.draw(self.screen)
-        #for e in self.enemies:
-        #    e.draw(self.screen)
-        if(self.selection != []):
-            for unit in self.selection:
-                unit.drawfocus(self.screen)
-        if((self.mouserect.width != 0) or (self.mouserect.height != 0)):
-            pygame.draw.rect(self.screen,(175,175,175),self.mouserect,2)
-
-        for b in self.bullets:
-            b.draw(self.screen)
+        self.map.draw(self.screen)
+        if self.mouseRect.width != 0 or self.mouseRect.height != 0:
+            pygame.draw.rect(self.screen,(175,175,175),self.mouseRect,2)
         
 g = Game()
 
--- trunk/variable.py
+++ trunk/variable.py
@@ -2,7 +2,9 @@ import pygame
 
 class Variable:
     def __init__(self, type = "", value = None):
+        # The variable's stored value
         self.value = value
+        # The type of variable it is (e.g., "bool", "integer", "unit", etc.)
         self.type = type
         
     def get_value(self):
